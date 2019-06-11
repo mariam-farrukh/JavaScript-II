83afba3e19ef84aa071d822eeeb5f29172664116
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-06-11 15:51:48 -0500

    Finished array methods problems

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -74,13 +74,18 @@ console.log(allCaps);
 //Get a list of runners with large sized shirts so they can choose a different size. 
 //Return an array named largeShirts that contains information about the runners that 
 //have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(function(currentValue){
+    // True or False
+    return currentValue.shirt_size = "L";
+    });
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. 
 //Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function(addedTotal, currentValue){
+    return addedTotal + currentValue.donation;
+  }, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -88,8 +93,21 @@ console.log(ticketPriceTotal);
 //I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  
 //Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 Capitalize all the last names to print them onto jerseys
+let lastAllCaps = runners.map(function(currentValue){
+    return currentValue.last_name.toUpperCase();
+});
+console.log(lastAllCaps); 
 
-// Problem 2
+// Problem 2 Compile a contact list of the runners Full name and email address
+let contactInfo = runners.forEach(function(currentValue) {
+    console.log(`${currentValue.first_name} ${currentValue.last_name} ${currentValue.email}`);
 
-// Problem 3
+    });
+console.log(contactInfo);
+
+// Problem 3 Compile a list of poeople to thank at companies.
+let thankPersonAtTheCompany = runners.filter(function(currentValue) {
+    console.log(`Thank you, ${currentValue.first_name} ${currentValue.last_name} at ${currentValue.company_name}`);
+});
+console.log(thankPersonAtTheCompany);
